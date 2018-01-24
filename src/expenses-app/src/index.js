f01085130ce78deab2be226330e91b1211882e40
Roberto <roberto@almendro.com.br>
2018-01-23 21:34:49 -0500

    added redux and initial pages to control authorization
    added redux and initial pages to control authorization

--- src/expenses-app/src/index.js
+++ src/expenses-app/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import thunkMiddleware from 'redux-thunk';
+import api from './middleware/api';
+import reducersApp from './reducers.jsx';
+
+
+let createStoreWithMiddleware  = applyMiddleware(thunkMiddleware, api)(createStore)
+
+let store = createStoreWithMiddleware(reducersApp)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
